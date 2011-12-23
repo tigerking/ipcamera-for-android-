51bd77b3b69df5c2b132a8a43d06083f7d99897f
achang.zhou@gmail.com <achang.zhou@gmail.com@c88b35bc-41e8-9fae-ddfb-a8de8c647189>
2011-12-23 13:41:07 +0000

    MP4 checking is OK and remove none version tracking file.

--- droidipcam/jni/ipcamera.h
+++ droidipcam/jni/ipcamera.h
@@ -1,14 +1,18 @@
 #ifndef _IPCAMERA_H_
 #define _IPCAMERA_H_
 
+#include <vector>
 #include <string>
 #include <jni.h>
 #include <android/log.h>
-#include "ipcamera.h"
+
 
 #define  LOG_TAG    "TEAONLY"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)  
 
+extern int begin_skip;
+extern std::vector<unsigned char> sps_data;
+extern std::vector<unsigned char> pps_data;
 
 int CheckMedia(const std::string mp4_file);
 
--- droidipcam/jni/mediacheck.cpp
+++ droidipcam/jni/mediacheck.cpp
@@ -1,5 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
 #include "ipcamera.h"
 
-int CheckMedia(const std::string mp4_file) {
-    return 1;
+int begin_skip = -1;
+std::vector<unsigned char> sps_data;
+std::vector<unsigned char> pps_data;
+
+/*
+   put_byte(pb, 1);      // version 
+   put_byte(pb, sps[1]); // profile 
+   put_byte(pb, sps[2]); // profile compat 
+   put_byte(pb, sps[3]); // level 
+   put_byte(pb, 0xff);   // 6 bits reserved (111111) + 2 bits nal size length - 1 (11)
+
+   put_byte(pb, 0xe1);   // 3 bits reserved (111) + 5 bits number of sps (00001) 
+
+
+   put_be16(pb, sps_size);
+   put_buffer(pb, sps, sps_size);
+   put_byte(pb, 1);      // number of pps 
+   put_be16(pb, pps_size);
+   put_buffer(pb, pps, pps_size);      
+*/
+
+void getHeader(FILE *fp) {
+    unsigned char temp_buffer[1024];
+    fread(temp_buffer, 1, 1024, fp);    
+
+    int sps_size = (temp_buffer[5] << 8) + temp_buffer[6];
+    for(int i = 0; i < sps_size; i++) {
+        sps_data.push_back( temp_buffer[7+i] );
+    }
+
+    int pps_size = (temp_buffer[8 + sps_size] << 8) + temp_buffer[9 + sps_size];
+    for(int i = 0; i < pps_size; i++) {
+        pps_data.push_back( temp_buffer[10 + sps_size + i] );
+    }
 }
+
+int CheckMedia(const std::string mp4_file) {
+    std::deque<unsigned char> mdat;
+    std::deque<unsigned char> avcC;
+
+    for(int i = 0; i < 4; i++) {
+        mdat.push_back(0);
+        avcC.push_back(0);
+    }
+    avcC.push_back(0);
+
+    begin_skip = -1;
+    FILE *fp = fopen( mp4_file.c_str(), "rb");
+    unsigned char c;
+
+    // 0. find av data(mdat) start position
+    int pos = 0;
+    while( !feof(fp) ) {
+        c = fgetc(fp);
+        pos ++;
+        mdat.push_back(c);
+        mdat.pop_front();
+        if (    mdat[0] == 'm'
+                && mdat[1] == 'd'
+                && mdat[2] == 'a'
+                && mdat[3] == 't') {
+            begin_skip = pos;
+            std::cout << "Found MDAT skipped = "<< pos << std::endl;
+            break; 
+        }
+    } 
+
+    if ( begin_skip < 0)
+        return 0;
+
+    sps_data.clear();
+    pps_data.clear();
+
+    // 1. get pps and sps data
+    while( !feof(fp) ) {
+        c = fgetc(fp);
+        avcC.push_back(c);
+        avcC.pop_front();
+        if (    avcC[0] == 'a'
+                && avcC[1] == 'v'
+                && avcC[2] == 'c'
+                && avcC[3] == 'C'
+                && avcC[4] == 0x01) {
+            std::cout << "Found sps and pps data" << std::endl;
+            getHeader(fp);
+            return 1;
+        }
+    }  
+
+    return 0;
+}
+
+#if 0
+int main(int argc, const char *argv[]) {
+    if ( argc > 1)
+        CheckMedia(argv[1]);
+}
+#endif
+
