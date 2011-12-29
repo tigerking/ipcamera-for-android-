fedaf6aac46b15c064d10fe936fc25529b7d056c
achang.zhou@gmail.com <achang.zhou@gmail.com@c88b35bc-41e8-9fae-ddfb-a8de8c647189>
2011-12-29 14:23:44 +0000

    Temp backup...

--- droidipcam/jni/mediaformat.cpp
+++ droidipcam/jni/mediaformat.cpp
@@ -12,6 +12,7 @@ const int MAX_VIDEO_PACKAGE = 384*1024;
 class FormatTask : public talk_base::MessageHandler {  
 public:
     FormatTask(int ifd, int ofd) {
+        frame_num_length = -1;
         infd = ifd;
         outfd = ofd;
         media_buffer = new MediaBuffer(32, 120, MAX_VIDEO_PACKAGE, 1024); 
@@ -26,10 +27,13 @@ public:
 
 protected:    
     virtual void OnMessage(talk_base::Message *msg);
+    int checkSingleSliceNAL(const std::deque<unsigned char> &pattern );
 
 private:
     int infd;
     int outfd;    
+
+    int frame_num_length;
 
     MediaBuffer *media_buffer;
 };
@@ -62,6 +66,7 @@ void FormatTask::OnMessage(talk_base::Message *msg) {
     }
 }
 
+/*
 static int fillBuffer(int sck, unsigned char *buf, unsigned int len) {
   while(len > 0) {
     int ret = read(sck, buf, len);
@@ -74,8 +79,9 @@ static int fillBuffer(int sck, unsigned char *buf, unsigned int len) {
   }
   return len;
 }
+*/
 
-static int checkFirstSliceNAL(const std::deque<unsigned char> &pattern ) {   
+int FormatTask::checkSingleSliceNAL(const std::deque<unsigned char> &pattern ) {   
     
     // 1. first we check NAL's size, valid size should less than 192K 
     if ( pattern[0] != 0x00)
@@ -96,34 +102,75 @@ static int checkFirstSliceNAL(const std::deque<unsigned char> &pattern ) {
     }  
 
     // 3. checking slice type, first_mb, and frame_num
-    int frame_num = -1;
+    // Only following pattens are supported. 
+    //  
+    // I Frame: b 1011 1***   (first_mb = 0, slice_type = 2, pps_id = 0)   
+    // I Frame: b 1000 1000 1 (first_mb = 0, slice_type = 7, pps_id = 0)
+    // P Frame: b 111* ****   (first_mb = 0, slice_type = 0, pps_id = 0)
+    // P Frame: b 1001 011*   (first_mb = 0, slice_type = 5, pps_id = 0)
+    // 
     int slice_type = -1;    // P = 0, I = 1
+    int frame_num_skip = -1;
+    int frame_num = -1;
     if ( (pattern[5] & 0xF8 ) == 0xB8) {
         slice_type = 1;
-    } else if ( (pattern[5] & 0xFF) == 0x80) {
+        frame_num_skip = 5;
+    } else if ( ((pattern[5] & 0xFF) == 0x80) 
+                && ((pattern[6] & 0x80) == 0x80) ) {
         slice_type = 1;
+        frame_num_skip = 9;
     } else if ( (pattern[5] & 0xE0) == 0xE0) {
         slice_type = 0;
+        frame_num_skip = 3;
     } else if ( (pattern[5] & 0xFE) == 0x9A) {
         slice_type = 0;
+        frame_num_skip = 7;
     }
     if ( slice_type == -1)
         return -1;
 
+    if ( frame_num_length == -1) {
+        frame_num_length = 0;
+        frame_num = 0;
+    } else if ( frame_num_length == 0) {
+        unsigned int bits = (pattern[5] << 24) + (pattern[6] << 16) + (pattern[8] << 8) + pattern[9];
+        bits = bits << frame_num_skip;
+        for(int i = 0; i < (31 - frame_num_skip); i++) {
+            if ( bits & 0x80000000 ) {
+                frame_num_length = i + 1;    
+            }
+            bits = bits << 1;
+        }
+    }
     
+    {
+        char temp[128];
+        snprintf(temp, 128, "frame num length = %d, num = %d", frame_num_length, frame_num);
+        LOGD(temp);
+    }
+
+    if ( slice_type == 0) {
+        LOGD("Find new P frame....\n");
+    } else {
+        LOGD("Find new I frame....\n");
+    }
+
+    return 0;    
 }
 
 void FormatTask::doFormat() {
     std::deque<unsigned char> video_check_pattern;
-    video_check_pattern.resize(6, 0x00);
+    video_check_pattern.resize(8, 0x00);
     
     unsigned char *buf;
     buf = new unsigned char[1024*512];
 
     unsigned char current_byte;
+    /*
     unsigned int vpkg_len = 0;
     unsigned int slice_len = 0;
-   
+    */
+
     while(1) {
 
         // find video slice data from es streaming 
@@ -132,39 +179,8 @@ void FormatTask::doFormat() {
         video_check_pattern.pop_front();
         video_check_pattern.push_back(current_byte);
 
-        vpkg_slice_len =  checkSliceNAL( video_check_pattern );
+        checkSingleSliceNAL( video_check_pattern );
 
-        if ( vpkg_len == 0)
-            continue;
-        
-        if ( vpkg_len > (unsigned int)MAX_VIDEO_PACKAGE )
-              LOGD("ERROR: Big video frame....");
-        
-        // reading video package.
-        for(int i = 0; i < 6; i++) {
-            buf[i] = video_check_pattern[i];
-            video_check_pattern[i] = 0;
-        }
-        
-        // this is first slice
-        if ( fillBuffer(camera_sink_, &buf[6], vpkg_len - 2) < 0)
-              break;
-        vpkg_len += 4;
-
-        // this is second slice , check it is valid?
-        if ( fillBuffer(camera_sink_, &buf[vpkg_len], 6) < 0)
-             break;
-        second_slice_len = checkSecondSlice( &buf[vpkg_len] );
-        if ( second_slice_len == 0)
-            continue;
-        
-        if ( fillBuffer(camera_sink_, &buf[vpkg_len+6], second_slice_len - 2) < 0)
-            break;
-
-        vpkg_len += (second_slice_len + 4);
-        
-        vts++;
-        SignalNewPackage(buf, vpkg_len, vts, MEDIA_TYPE_VIDEO);
     }
     delete buf;
 
